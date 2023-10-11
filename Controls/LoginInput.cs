ee0e2e4243c07dd6e41e5a57d8b168fa0ebd3d65
Alexey Kravchenko <AlexeyKravchenko@coherentsolutions.com>
2023-10-11 16:54:17 +0300

    Added login test. Added class with By variables.

---
+++ Controls/LoginInput.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+
+namespace Task20.Controls
+{
+    public class LoginInput
+    {
+        public By ByXpath = By.XPath("//input[@name='username']");//works
+
+        public By ByClassName = By.ClassName("form-control"); //does not work (6 matches)
+
+        public By ByCssSelector = By.CssSelector("#inputEmail"); //works
+
+        public By ById = By.Id("inputEmail"); //works
+
+        public By ByName = By.Name("username"); //works
+
+        public By ByTagName = By.TagName("input"); //does not work (11 matches)
+
+        public By ByLinkedText = By.LinkText("LOGIN"); //works
+
+        public By ByPartialLinkText = By.PartialLinkText("LOGIN"); //works
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,16 +1,59 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Task20.Controls;
+
 namespace Task20
 {
     public class Tests
     {
+        private WebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            LoginInput login = new LoginInput();
+
+            //// Doing login from the demo page. NOTE: this page is often unreachable
+            //driver.Navigate().GoToUrl("https://phptravels.com/demo");
+            //var loginBtn = driver.FindElement(login.ByLinkedText);
+            //loginBtn.Click();
+
+            //driver.SwitchTo().Window(driver.WindowHandles.Last());
+
+            driver.Navigate().GoToUrl("https://phptravels.org/");
+
+            var loginInput = driver.FindElement(login.ByXpath);
+            loginInput.SendKeys("mycoolemail@gmail.com");
+
+            var passwordInput = driver.FindElement(By.XPath("//input[@name='password']"));
+            passwordInput.SendKeys("z!SuiDzaw9oj");
+
+            // Can't automate captcha, so just verify that alert is displayed
+            //var captchaChk = driver.FindElement(By.XPath("//div[@id='google-recaptcha-domainchecker1']"));
+            //captchaChk.Click();
+
+            var loginButton = driver.FindElement(By.XPath("//button[@id='login']"));
+            loginButton.Click();
+
+            var alertLabel = driver.FindElement(By.XPath("//div[@class='alert alert-danger']"));
+
+            // Login process won't complete because it is needed to handle captcha input, so I have to verify that alert is displayed
+            Assert.IsTrue(alertLabel.Displayed, "Error: alert is not displayed!");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            driver.Dispose();
         }
     }
 }
